a1e0b3947579bee37a9be9e36311c8742a913b97
DevRomu <orinarhomill@gmail.com>
2023-09-18 17:01:16 -0500

    Added more code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Connect to JSoup
+        Document doc = Jsoup.connect("https://quotes.toscrape.com/").get();
+
+        // Do something with the parsed HTML document
     }
 }
