7bc6086ff2599c57e2230472ca070e0f82beb192
DevRomu <orinarhomill@gmail.com>
2023-09-19 10:18:31 -0500

    Tried to optimize imports, but the errors keep appearing

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,22 +1,42 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
 
 public class MainActivity extends AppCompatActivity {
 
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Connect to JSoup
-        Document doc = Jsoup.connect("https://quotes.toscrape.com/").get();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // Connect to the website
+                    Connection.Response response = Jsoup.connect("https://quotes.toscrape.com/").execute();
 
-        // Do something with the parsed HTML document
+                    // Check if the request was successful (HTTP 200)
+                    if (response.statusCode() == 200) {
+                        Document doc = response.parse();
+
+                        // Do something with the parsed HTML document
+
+                        // For example, print the HTML content
+                        System.out.println(doc.html());
+                    } else {
+                        System.err.println("Failed to fetch the webpage. Status code: " + response.statusCode());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
