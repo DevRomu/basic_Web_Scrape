3175f338e08dfdcec42a9152f3d128d7b42aa9b3
CCianfloneDev <colecianflone123@gmail.com>
2023-09-22 10:16:26 -0500

    There is a safety feature which stops you from doing api calls on the UI thread, created a new thread for the API call.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,22 +1,17 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button scrapeButton;
     private TextView scrapedTextView;
 
     @Override
@@ -24,21 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scrapeButton = findViewById(R.id.button);
+        Button scrapeButton = findViewById(R.id.button);
         scrapedTextView = findViewById(R.id.textView);
 
-        scrapeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    scrapeTextFromUrl();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
+        scrapeButton.setOnClickListener(v -> scrapeTextFromUrl());
+    }
 
-            private void scrapeTextFromUrl() throws IOException {
-                // Get the URL to scrape from the website.
+    private void scrapeTextFromUrl() {
+        // Start a new thread for network operations as it will crash if you perform network operations on the ui thread
+        new Thread(() -> {
+            try {
+                // URL to scrape from the website.
                 String url = "https://quotes.toscrape.com/";
 
                 // Create a Jsoup Document Object from the URL.
@@ -48,13 +39,17 @@ public class MainActivity extends AppCompatActivity {
                 Elements quoteElements = doc.select("div.quote span.text");
 
                 // Extract and concatenate the quotes.
-                StringBuilder scrapedText = new StringBuilder();
+                final StringBuilder scrapedText = new StringBuilder();
                 for (Element element : quoteElements) {
                     scrapedText.append(element.text()).append("\n\n");
                 }
 
-                scrapedTextView.setText(scrapedText.toString());
+                // Update the UI on the main thread
+                runOnUiThread(() -> scrapedTextView.setText(scrapedText.toString()));
+            } catch (IOException e) {
+                e.printStackTrace();
+                // Handle the exception here (e.g., show an error message to the user)
             }
-        });
+        }).start();
     }
 }
